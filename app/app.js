b109aa06ea2037a1e54bed0f4ebef7dc2234e597
venkatram64 <venkat.veerareddy@hotmail.com>
2017-10-21 22:02:06 -0700

    added code

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
+import $ from "jquery";
 require('./style.css');
+
+$("a.stack").on("click", function(){
+    $("article").addClass("stack");
+});
+$("a.grid").on("click", function(){
+    $("article").removeClass("stack");
+});
